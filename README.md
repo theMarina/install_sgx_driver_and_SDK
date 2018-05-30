763062898b5812d5457f2c8cab1031cfecd10c8d
Marina Minkin <mini@cs.technion.ac.il>
2018-05-30 16:54:10 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository proveide scripts to install the linux SGX enviroment.
+These scripts do exactly what the official documentation sais.
+Tested partially on a clean Ubuntu 16.04 install.
---
+++ install_SGX_driver.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/intel/linux-sgx-driver.git
+cd linux-sgx-driver
+sudo apt-get install linux-headers-$(uname -r)
+make
+sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"    
+sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
+sudo /sbin/depmod
+sudo /sbin/modprobe isgx
