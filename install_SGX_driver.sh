12942cbcd4fd3b6fd6d868e212511ed9739b5922
Marina Minkin <mini@cs.technion.ac.il>
2018-05-30 16:55:31 +0300

    added sucess message

--- install_SGX_driver.sh
+++ install_SGX_driver.sh
@@ -1,3 +1,4 @@
+set -e
 git clone https://github.com/intel/linux-sgx-driver.git
 cd linux-sgx-driver
 sudo apt-get install linux-headers-$(uname -r)
@@ -7,3 +8,5 @@ sudo cp isgx.ko "/lib/modules/"`uname -r`"/kernel/drivers/intel/sgx"
 sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo isgx >> /etc/modules"    
 sudo /sbin/depmod
 sudo /sbin/modprobe isgx
+
+echo "SGX driver succesfully installed"
