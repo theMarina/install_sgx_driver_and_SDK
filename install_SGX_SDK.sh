a5c11c3bd130f0c87fe37efc0fae5ecc3bb94e2c
Marina Minkin <minkin@umich.edu>
2019-12-28 13:15:09 -0500

    updated the script for new SDK

--- install_SGX_SDK.sh
+++ install_SGX_SDK.sh
@@ -10,16 +10,19 @@ sudo apt-get install alien
 #sudo alien --scripts iclsClient-1.45.449.12-1.x86_64.rpm
 #sudo dpkg -i iclsclient_1.45.449.12-2_amd64.deb
 sudo apt-get install uuid-dev libxml2-dev cmake pkg-config
-#sudo apt install ocamlbuild
+sudo apt install ocamlbuild
 
 make sdk_install_pkg
-make psw_install_pkg
 
 cd linux/installer/bin/
-sudo ./sgx_linux_x64_psw_*.bin
 sudo ./sgx_linux_x64_sdk_*.bin << EOF
 no
 /opt/intel
 EOF
+cd -
+
+make psw_install_pkg
+cd linux/installer/bin/
+sudo ./sgx_linux_x64_psw_*.bin
 
 echo "SGX SDK succesfully installed!"
